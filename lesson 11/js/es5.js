2d0f95469a9c12327a7bfa2785f1d6c06ea5268e
alex <realcrazyn@yandex.ru>
2020-12-14 17:08:36 +0300

    Lesson 11

---
+++ lesson 11/js/es5.js
@@ -0,0 +1,90 @@
+
+// 3)
+// function User(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function() {
+//         console.log('Hello! ' + this.name);
+//     }
+// }
+
+// User.prototype.exit = function(name) {
+//     console.log('User ' + this.name + ' left!');
+// }
+
+// let ivan = new User('Ivan', 25);
+// let alex = new User('Alex', 20);
+
+// console.log(ivan);
+// console.log(alex);
+
+// ivan.exit();
+
+//1)
+// 'use strict';
+// function showThis(a,b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b;
+//     }
+//     console.log(sum());
+// }
+// showThis(5,6);
+// showThis(5,5);
+
+//2)
+// let obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//         function shout(){
+//             console.log(this);
+//         }
+//         shout();
+//     }
+// };
+// obj.sum();
+
+
+
+//4)
+// let user = {
+//     name: 'John'
+
+// };
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
+
+// console.log(sayName.call(user, 'Smith')); //принимает аргументы ввиде строки
+// console.log(sayName.apply(user, ['Snow']));//принимает аргументы ввиде массива
+
+// function count(number) {
+//     return this * number;
+// }
+
+// let double = count.bind(2);  //this станет тем  что записали в (2)  
+// console.log(double(3));
+// console.log(double(10));
+
+
+//1) Просто вызов функции - без стрикта получим window/ с 'use strict' получим undefined
+// 2) Метод объекта - this = самому объекту
+// 3) Конструктор (new) - this = новый созданный объект
+// 4) указание конкретного контекста - call, apply, bind
+
+let btn = document.querySelector('button');
+btn .addEventListener('click', function() {
+    console.log(this);
+    this.style.backgroundColor = 'red';
+    function showThis() {
+        console.log(this);
+    }
+    showThis();
+})
+
+
---
+++ lesson 11/js/es6.js
@@ -0,0 +1,20 @@
+// class User {
+//     constructor(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     }
+//     hello() {
+//         console.log(`Hello! ${this.name}`);
+//     }   
+//     exit() {
+//         console.log(`This user ${this.name} left`);
+//     }
+// }   
+
+// let ivan = new User('Ivan', 25);
+// let alex = new User('Alex', 20);
+// console.log(ivan);
+// console.log(alex);
+// ivan.exit();
+// alex.hello();
